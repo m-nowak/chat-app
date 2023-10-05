34c05f52e22b8a60e5a141e7ee82d209a6d8e6a3
Michal <cwiarinio@googlemail.com>
2023-10-05 11:43:29 +0100

    12

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { chunkSizeWarningLimit: 1600 },
   server: {
     port: 3000,
     proxy: {
