a1e910c903f05be32eb8f30976aafaf1645694b8
Michal <cwiarinio@googlemail.com>
2023-10-05 14:25:10 +0100

    14

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -87,7 +87,16 @@ const HomePage = () => {
     const getConversations = async () => {
       try {
         const res = await fetch(
-          "https://chat-app-api-tvkc.onrender.com/api/messages/conversations"
+          "https://chat-app-api-tvkc.onrender.com/api/messages/conversations",
+          {
+            method: "GET",
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
         );
         const data = await res.json();
         if (data.error) {
