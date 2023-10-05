46bb4130cca1cd2587931a4222e270812645e7fb
Michal <cwiarinio@googlemail.com>
2023-10-05 13:50:10 +0100

    23

--- server/utils/helpers/generateTokenAndSetCookie.js
+++ server/utils/helpers/generateTokenAndSetCookie.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     maxAge: 15 * 24 * 60 * 60 * 1000,
-    sameSite: "strict",
+    sameSite: "lax",
   });
 
   return token;
