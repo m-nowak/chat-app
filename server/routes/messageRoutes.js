c67c37fd19fbb6354853ac425c9869a40837bff7
Michal <cwiarinio@googlemail.com>
2023-10-05 13:22:37 +0100

    21

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import { getMessages, sendMessage, getConversations } from "../controllers/messageController.js";
+import {
+  getMessages,
+  sendMessage,
+  getConversations,
+} from "../controllers/messageController.js";
 
 const router = express.Router();
 
-router.get("/conversations", protectRoute, getConversations);
-router.get("/:otherUserId", protectRoute, getMessages);
-router.post("/", protectRoute, sendMessage);
+router.get("/conversations", getConversations);
+router.get("/:otherUserId", getMessages);
+router.post("/", sendMessage);
 
 export default router;
