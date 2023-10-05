44e1c2ae3ea5d1c0645535d00159a3ca1d168d90
Michal <cwiarinio@googlemail.com>
2023-10-05 12:59:01 +0100

    17

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import userRoutes from "./routes/userRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { app, server } from "./socket/socket.js";
 
+const cors = require("cors");
+
 dotenv.config();
 
 connectDB();
@@ -15,6 +17,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
