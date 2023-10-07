0062e52460790149cee9b1a70258f74e8c84bc51
Michal <cwiarinio@googlemail.com>
2023-10-07 14:31:16 +0100

    vite.config changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { chunkSizeWarningLimit: 1600 },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      external: ["react", "react-router", "react-router-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
   server: {
     port: 3000,
     proxy: {
