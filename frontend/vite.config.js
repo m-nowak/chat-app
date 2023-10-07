d4ff77091d3f0f5935691edd29e389ae18826a50
Michal <cwiarinio@googlemail.com>
2023-10-07 14:50:44 +0100

    vite.config changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // svgrPlugin(),
+  ],
   build: {
-    chunkSizeWarningLimit: 1600,
     lib: {
-      entry: resolve(__dirname, "src/lib/index.js"),
-      name: "core-package",
-      fileName: "index",
+      // entry: resolve(__dirname, "src/lib/main.js"),
+      // name: "core-package",
+      // fileName: "index",
     },
     rollupOptions: {
-      external: ["react", "react-router", "react-router-dom"],
+      external: ["react"],
       output: {
         globals: {
           react: "React",
